dd3332ab5a52989c1d98f4f6c19c19e4f0c4f117
johnny090807 <33625576+johnny090807@users.noreply.github.com>
2020-10-26 09:27:24 +0100

    added login and register rest-api

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from '../login/login-service.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService : LoginService,
+              private router: Router) { }
 
   ngOnInit(): void {
+
+  }
+  loggedIn(){
+    return this.loginService.loggedInUser;
+  }
+  logout(){
+    this.router.navigate(['/Home']);
+    this.loginService.loggedInUser = null;
   }
 
 }
