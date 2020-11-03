35075733a16bc656cace56dcaef0af99152b79a2
RBLeon <57948143+RBLeon@users.noreply.github.com>
2020-11-03 11:20:15 +0100

    added the link to spring through a http get call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -6,4 +10,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  title = "CrsX test"
+
+  constructor(private http: HttpClient){
+    http.get(`http://localhost:8080/pageTitles/1`, {responseType: 'text'})
+    .pipe(
+      catchError((err)=> {
+        console.log(err);
+        return of('404 not Found');
+      })
+    ).subscribe(pageTitle => this.title = pageTitle);
+  }
 }
