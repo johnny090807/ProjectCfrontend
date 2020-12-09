11d80d7d9d55a6572718abefc9317eefb8c864f8
Amy Fang <ianmeesters@hotmail.com>
2020-12-09 10:47:57 +0100

    :D <3

---
+++ src/app/admin-cars/admin-cars.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-cars',
+  templateUrl: './admin-cars.component.html',
+  styleUrls: ['./admin-cars.component.scss']
+})
+export class AdminCarsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
