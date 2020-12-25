b00067d6c37a2fd04e563632a4954670d4395d4e
Amy Fang <46504388+ianmeesters@users.noreply.github.com>
2020-12-25 15:24:19 +0100

    dis new

--- src/app/admin-cars/admin-cars.component.ts
+++ src/app/admin-cars/admin-cars.component.ts
@@ -41,4 +41,14 @@ export class AdminCarsComponent implements OnInit {
       });
   }
 
+  getCar(id) {
+    let IpLink = localStorage.getItem('serverIp');
+    this.http.get(IpLink + '/api/getCarById?Id=' + id, { responseType: 'json' })
+      .subscribe((response) => {
+        this.cars = [];
+        this.cars.push(response[0]);
+        console.log(this.cars);
+      });
+  }
+
 }
