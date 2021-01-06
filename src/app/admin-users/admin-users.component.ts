db7de1e10735ae7a2d91405224f8ca20d8edd4b0
Amy Fang <46504388+ianmeesters@users.noreply.github.com>
2021-01-06 11:02:43 +0100

    added more admin shit!

--- src/app/admin-users/admin-users.component.ts
+++ src/app/admin-users/admin-users.component.ts
@@ -5,6 +5,8 @@ import { ignoreElements, catchError, retry } from 'rxjs/operators';
 import { Observable, of, throwError } from 'rxjs';
 import { Router } from '@angular/router';
 import { AdminUsersService } from './admin-users.service';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-admin-users',
@@ -15,15 +17,23 @@ import { AdminUsersService } from './admin-users.service';
 export class AdminUsersComponent implements OnInit {
   public users: Array<User>;
   public user: User;
+  public selection: boolean;
   constructor(
     private http: HttpClient,
     private router: Router,
-    private adminUsersService: AdminUsersService
+    private adminUsersService: AdminUsersService,
+    private sanitizer: DomSanitizer
   ) { }
 
 
   ngOnInit(): void {
     this.users = this.adminUsersService.returnUsers();
+    this.selection = false;
+  }
+
+  noUser() {
+    this.selection = false;
+    this.users = [];
   }
 
 
@@ -37,6 +47,19 @@ export class AdminUsersComponent implements OnInit {
           this.users.push(this.user);
         }
 
+      });
+  }
+
+  getUser() {
+    let id = (<HTMLInputElement>document.getElementById("id")).value;
+    let IpLink = localStorage.getItem('serverIp');
+    this.http.get(IpLink + '/api/getUserById?id=' + id, { responseType: 'text' })
+      .subscribe((response) => {
+        this.users = [];
+        console.log(response);
+        this.users.push(JSON.parse(response));
+        console.log(this.users);
+        this.selection = true;
       });
   }
 
