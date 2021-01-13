8a5a33d5ead1dd4a89166affb659cd3060f19a83
Amy Fang <46504388+ianmeesters@users.noreply.github.com>
2021-01-13 23:01:47 +0100

    verEngelsen

--- src/app/rent/car.service.ts
+++ src/app/rent/car.service.ts
@@ -8,31 +8,21 @@ import { Car } from './car.model';
   providedIn: 'root'
 })
 export class CarService {
-  public cars:Array<Car>= []
+  public cars: Array<Car> = []
   public images = [];
-  // public cars:Array<Car>= [
-  //   {id: 1, brand: "Manual", model: "BMW", price: 30, location: "Den Haag", doors: 3,  imageUrl: "../../assets/car2.png", mileage: 102300, carAge: 2015},
-  //   {id: 2, brand: "Automatic", model: "Dikke BMW", price: 60, location: "Voorburg", doors: 3,  imageUrl: "../../assets/car2.png", mileage: 102300, carAge: 2015},
-  //   {id: 3, brand: "Automatic", model: "Dikke BMW", price: 50, location: "Rotterdam", doors: 3, imageUrl: "../../assets/car2.png", mileage: 102300, carAge: 2015},
-  //   {id: 4, brand: "Automatic", model: "BMW", price: 80, location: "Hellevoetsluis", doors: 3,  imageUrl: "../../assets/car2.png", mileage: 102300, carAge: 2015},
-  //   {id: 5, brand: "Manual", model: "BMW", price: 25, location: "Utrecht",  doors: 3, imageUrl: "../../assets/car2.png", mileage: 102300, carAge: 2015},
-  //   {id: 6, brand: "Manual", model: "Dikke BMW", price: 35, location: "Rozenburg", doors: 3, imageUrl: "../../assets/car2.png", mileage: 102300, carAge: 2015},
-  //   {id: 7, brand: "Manual", model: "Dikke BMW", price: 40, location: "Wateringen", doors: 3, imageUrl: "../../assets/car2.png", mileage: 102300, carAge: 2015},
-  //   {id: 8, brand: "Automatic", model: "BMW", price: 50, location: "Voorburg", doors: 3,  imageUrl: "../../assets/car2.png", mileage: 102300, carAge: 2015}
-  // ]
   constructor(private http: HttpClient,
-              private sanitizer: DomSanitizer) {
+    private sanitizer: DomSanitizer) {
     this.getAllCars();
     this.storeImages()
-   }
+  }
 
-  public returnCars(){
+  public returnCars() {
     return this.cars;
   }
 
-  async getAllCars(){
+  async getAllCars() {
     let IpLink = localStorage.getItem('serverIp');
-    let headers = new HttpHeaders({'Content-Type': 'application/json'});
+    let headers = new HttpHeaders({ 'Content-Type': 'application/json' });
     this.http.get(IpLink + '/api/getCars', { headers, responseType: 'json' })
       .subscribe((res: Array<Car>) => {
         this.cars = res;
@@ -40,18 +30,20 @@ export class CarService {
         error => console.log(error))
     // return returnthing
   }
-  public storeImages(){
-    let headers = new HttpHeaders({'Content-Type': 'image/png',
-    'Accept' : 'image/png'});
+  public storeImages() {
+    let headers = new HttpHeaders({
+      'Content-Type': 'image/png',
+      'Accept': 'image/png'
+    });
     let IpLink = localStorage.getItem('serverIp');
     console.log()
     this.cars.forEach(car => {
-      this.http.get(IpLink + '/api/getImageByPath?path='+car.imagePath+'', {headers, observe : 'response', responseType: 'text'})
-      .subscribe((response) => {
-      let objectURL = 'data:image/png;base64,' + response.body;
-      car.imageUrl = objectURL
-      },
-      error => console.log(error))
+      this.http.get(IpLink + '/api/getImageByPath?path=' + car.imagePath + '', { headers, observe: 'response', responseType: 'text' })
+        .subscribe((response) => {
+          let objectURL = 'data:image/png;base64,' + response.body;
+          car.imageUrl = objectURL
+        },
+          error => console.log(error))
     });
   }
 }
