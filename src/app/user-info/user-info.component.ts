5b3b329b7e8ec46ddfaebe28ef70ed3ad653a5e8
JordyLaker <60740747+JordyLaker@users.noreply.github.com>
2021-01-13 14:38:54 +0100

    update

--- src/app/user-info/user-info.component.ts
+++ src/app/user-info/user-info.component.ts
@@ -1,6 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient ,HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { LoginService } from '../login/login-service.service';
+import { User } from '../login/user.model';
+
 
 @Component({
   selector: 'app-user-info',
@@ -10,7 +13,13 @@ import { ActivatedRoute } from '@angular/router';
 export class UserInfoComponent implements OnInit {
   public reservations = [];
   constructor(private http: HttpClient,
+    private loginService: LoginService,
               private activatedRoute: ActivatedRoute) { }
+
+   NewPW="";
+   ConfirmNewpw="";
+   popupPW=false;
+   loggedInUser : User;
 
   ngOnInit(): void {
     this.getReservationsByUserId()
@@ -26,6 +35,27 @@ export class UserInfoComponent implements OnInit {
     .subscribe((response: Array<any>) => {
       this.reservations = response;
     })
+  }
+  test(){
+    this.NewPW=this.loggedInUser.password;
+  }
+
+  PwchangeConfirm(value1 , Value2){
+    if(value1 == Value2){
+      this.NewPW= value1;
+      console.log(this.NewPW);
+      this.loggedInUser.password = this.NewPW;
+      this.ChangePassword(this.loggedInUser.id,this.loggedInUser);
+    }
+  }
+  ChangePassword(ID,user:User){
+    let IPlink= localStorage.getItem('serverIp');
+    let body = JSON.stringify(User);
+    let headers = new HttpHeaders({'Content-Type': 'application/json'});
+    this.http.patch(IPlink+'/api/updateUser?id='+ID+body,{headers, responseType: 'text'})
+      .subscribe(res =>console.log(res) );
+
+
   }
 
 }
